12b0faf8ca2ef7152c1a18ef4d6a156371de8473
slhommed <slhommedieu@pivotal.io>
2016-11-30 15:35:41 -0700

    Support cfapp

---
+++ scripts/cf_deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash 
+
+#app_context_path=""
+domain="cfapps.io"
+host_name="pcf-autoscaler-and-scheduler-personas"
+app_name="persona_app"
+
+#Assumes you have done the following:
+# git clone https://github.com/slhommed/pcf-autoscaler-and-scheduler-personas
+#cf login -a http://api.run.pivotal.io
+#cf target -o <target_org> -s <target_space>
+##
+
+bundle install
+bundle exec middleman build
+mkdir -p ./cfapp ### /$docs_app_context_path
+cp -rf ./build/* ./cfapp/ ### /$docs_app_context_path
+cf push $app_name -m 64M -b https://github.com/cloudfoundry/staticfile-buildpack.git -d $domain --hostname $host_name -p ./cfapp
